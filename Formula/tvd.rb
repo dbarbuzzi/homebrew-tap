db5c8073d6190f2b640c68e348a8f5d34c753fcd
dbarbuzzi <dbarbuzzi@gmail.com>
2018-10-23 15:31:08 -0400

    Brew formula update for tvd version v0.4.2

--- Formula/tvd.rb
+++ Formula/tvd.rb
@@ -1,9 +1,9 @@
 class Tvd < Formula
   desc "Download Twitch VODs"
   homepage "https://github.com/dbarbuzzi/tvd"
-  url "https://github.com/dbarbuzzi/tvd/releases/download/v0.4.1/tvd_0.4.1_macOS_64bit.tar.gz"
-  version "0.4.1"
-  sha256 "e9e363597149da88dff6339d8ea807eae31d59fb5067c1aa8e2190600d712a6e"
+  url "https://github.com/dbarbuzzi/tvd/releases/download/v0.4.2/tvd_0.4.2_macOS_64bit.tar.gz"
+  version "0.4.2"
+  sha256 "571a4ea44af4a1e7e699997c74a768b3af6e1bb2354534f4dcf7c2a0bb9a535c"
 
   def install
     bin.install "tvd"
